3d5085266edf8b912dcb452948e5be7cbe3cdf8a
jonathanlunt <jonathan.lunt@gmail.com>
2019-02-26 22:26:52 -0800

    Coverage testing for multiple environments

--- example/main.py
+++ example/main.py
@@ -1,6 +1,24 @@
+import six
+
+
+def only_covered_in_py2():
+    print("Ran py2 function!")
+
+
+def only_covered_in_py3():
+    print("Ran py3 function!")
+
+
 def main():
+    if six.PY2:
+        only_covered_in_py2()
+    if six.PY3:
+        only_covered_in_py3()
     return 1
 
 
 def uncovered():
+    pass
+    pass
+    pass
     pass
